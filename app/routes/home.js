9d2d5e639e540e141318fc4fb457197bb4ccf23a
ding kai <46352678+DingKai0202@users.noreply.github.com>
2022-06-01 01:23:06 +0800

    主页

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,6 +2,10 @@ const Router = require('koa-router');
 const router = new Router();
 const HomeCtl = require('../controller/home');
 
+router.get('/test', (ctx) => {
+  ctx.body = '<h1>这是里交易者主页</h1>'
+})
+
 router.post('/upload', new HomeCtl().upload);
 
 module.exports = router;
